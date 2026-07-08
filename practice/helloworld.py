9dcc69311da2061f9188e6b117781d59c3ce70a8
Anuja Zade <anujazade18@gmail.com>
2026-07-08 18:52:58 +0530

    Hello world program

--- practice/helloworld.py
+++ practice/helloworld.py
@@ -1,1 +1,9 @@
+# print("Hello, World!   
+# 
+# --> worng code output generated  
+#  File "D:\AI-projects\Intern_Skill_plan_Zuigo\practice\helloworld.py", line 1
+#     print("Hello, World!   
+#           
+
+# corrected code:
 print("Hello, World!")
